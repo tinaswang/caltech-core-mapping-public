65eccba0332d3372fefaa3018ae4d7734435896f
Tina Wang <tswang@caltech.edu>
2018-05-13 07:50:37 -0700

    initial commit

--- app.py
+++ app.py
@@ -17,12 +17,14 @@ from collections import Counter as C
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
-# server = flask.Flask(__name__)
-# server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-# app = dash.Dash(__name__, server=server)
 
-app = dash.Dash(__name__)
-server = app.server
+
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
+
+# app = dash.Dash(__name__)
+# server = app.server
 
 
 # app = dash.Dash()
